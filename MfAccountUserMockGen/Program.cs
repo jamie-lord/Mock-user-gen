b53e69b9984eb0e25173d90c70f9c428c78f9e97
Jamie Lord <jamie@lord.technology>
2021-05-21 15:37:20 +0100

    Generate a CSV with n users to test import

---
+++ MfAccountUserMockGen/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Bogus;
+using CsvHelper;
+
+namespace MfAccountUserMockGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int count = 1000;
+
+            var users = new List<User>();
+
+            var faker = new Faker<User>()
+                .RuleFor(u => u.Email, s => s.Person.Email)
+                .RuleFor(u => u.FirstName, s => s.Person.FirstName)
+                .RuleFor(u => u.Surname, s => s.Person.LastName)
+                .RuleFor(u => u.ContactNumber, s => s.Person.Phone)
+                .RuleFor(u => u.StartDate, s => s.Date.Past(100).ToShortDateString());
+
+            users.AddRange(faker.Generate(count));
+
+            using (var writer = new StreamWriter($"{count}_users.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(users);
+            }
+        }
+    }
+
+    public class User
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string StartDate { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
